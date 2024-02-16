9cf34764308ff637868043308f8967960a973300
Jacek <jacek.klosinski@gmail.com>
2024-02-16 12:40:55 +0100

    feat: player setup response

--- src/in-memory-db.js
+++ src/in-memory-db.js
@@ -1,5 +1,21 @@
 
+import { randomUUID } from 'crypto';
 
 export const players = new Map();
-export const arePlayersSetUp = () => players.size === 2;
-export const setUpPlayer = ({ name, password, id }) => players.set(id, { name, password });
+const toPlayerResponse = (index, playerInput) => {
+    const parsedData = JSON.parse(playerInput.data);
+    return ({
+        ...playerInput,
+        data: JSON.stringify({
+            name: parsedData.name,
+            password: parsedData.password,
+            index
+        })
+    })
+}
+const savePlayer = ({ name, password}, index) => players.set(index, { name, password });
+export const setUpPlayer = (playerInput) => {
+    const index = randomUUID();
+    savePlayer(JSON.parse(playerInput.data), index);
+    return toPlayerResponse(index, playerInput)
+}
--- src/ws/ws.js
+++ src/ws/ws.js
@@ -1,19 +1,18 @@
 import { WebSocketServer } from 'ws';
-import { arePlayersSetUp, setUpPlayer, players } from '../in-memory-db.js';
+import { setUpPlayer, players } from '../in-memory-db.js';
 
 const wss = new WebSocketServer({ port: 3000 });
 
 wss.on('connection', function connection(ws) {
-    
-    console.log(players.size);
-    console.log('tu!');
+
     ws.on('error', console.error);
 
     ws.on('message', function message(data) {
-        console.log('received: %s', data);
-        console.log(arePlayersSetUp());
-        setUpPlayer(data);
-        console.log(players.size);
-        console.log(arePlayersSetUp());
+        const parsedData = JSON.parse(data);
+        if (parsedData.type === 'reg') {
+            const playerResponse = setUpPlayer(parsedData);
+            console.log('players.size', players.size);
+            ws.send(JSON.stringify(playerResponse));
+        }
     });
 });
