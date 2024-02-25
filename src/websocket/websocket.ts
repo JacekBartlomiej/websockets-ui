c059016f7e7c2c6fe6e3f69a515d8768c46cd8ad
Jacek <jacek.klosinski@gmail.com>
2024-02-25 13:03:45 +0100

    refactor: handlers

--- src/websocket/websocket.ts
+++ src/websocket/websocket.ts
@@ -35,55 +35,75 @@ wss.on("connection", function connection(ws) {
   connections[userId].on("message", function message(data: RawData) {
     const parsedData: Response = JSON.parse(data.toString());
     if (parsedData.type === TYPE.REG) {
-      const reg = toReg(parsedData, userId);
-      ws.send(JSON.stringify(reg));
-      const updateRooms = toUpdateRooms();
-      ws.send(updateRooms);
+      handleReg(ws, parsedData, userId);
     }
     if (parsedData.type === TYPE.CREATE_ROOM) {
-      const roomId = createRoom();
-      addUserToRoom(roomId, userId);
-      const updateRooms = toUpdateRooms();
-      ws.send(updateRooms);
+      handleCreateRoom(ws, userId);
     }
     if (parsedData.type === TYPE.ADD_USER_TO_ROOM) {
-      const roomId = toRoomId(parsedData.data);
-      const roomUsers: RoomUser[] = rooms.get(roomId)!;
-      if (roomUsers.length < 2) {
-        addUserToRoom(roomId, userId);
-        const updateRooms = toUpdateRooms();
-        ws.send(updateRooms);
-        if (roomUsers.length === 2) {
-          const gameId = randomUUID();
-          const createGame = toRequest(
-            TYPE.CREATE_GAME,
-            toCreateGame(userId, gameId)
-          );
-          saveGame(
-            gameId,
-            roomUsers.map((roomUser) => roomUser.index) as [UUID, UUID]
-          );
-          roomUsers.forEach(({ index }) => connections[index].send(createGame));
-        }
-      } else {
-        console.error("There can be maximum 2 players in one room");
-      }
+      handleAddUserToRoom(ws, parsedData, userId);
     }
     if (parsedData.type === TYPE.ADD_SHIPS) {
-      const gameId = toGameId(parsedData.data);
-      const playerShips = toShips(parsedData.data);
-      saveShips(gameId, userId, playerShips);
-      if (ships.get(gameId)?.size === 2) {
-        const players: [UUID, UUID] = games.get(gameId)!;
-        players.forEach((playerId) => {
-          const playerShips = ships.get(gameId)?.get(playerId)!;
-          const startGame = toRequest(
-            TYPE.START_GAME,
-            toStartGame(userId, playerShips)
-          );
-          connections[playerId].send(startGame);
-        });
-      }
+      handleAddShips(ws, parsedData, userId);
     }
   });
 });
+
+const handleReg = (ws: WebSocket, parsedData: Response, userId: UUID): void => {
+  const reg = toReg(parsedData, userId);
+  ws.send(JSON.stringify(reg));
+  const updateRooms = toUpdateRooms();
+  ws.send(updateRooms);
+};
+
+const handleCreateRoom = (ws: WebSocket, userId: UUID): void => {
+  const roomId = createRoom();
+  addUserToRoom(roomId, userId);
+  const updateRooms = toUpdateRooms();
+  ws.send(updateRooms);
+};
+
+const handleAddUserToRoom = (
+  ws: WebSocket,
+  parsedData: Response,
+  userId: UUID
+): void => {
+  const roomId = toRoomId(parsedData.data);
+  const roomUsers: RoomUser[] = rooms.get(roomId)!;
+  if (roomUsers.length < 2) {
+    addUserToRoom(roomId, userId);
+    const updateRooms = toUpdateRooms();
+    ws.send(updateRooms);
+    if (roomUsers.length === 2) {
+      const gameId = randomUUID();
+      const createGame = toRequest(
+        TYPE.CREATE_GAME,
+        toCreateGame(userId, gameId)
+      );
+      saveGame(
+        gameId,
+        roomUsers.map((roomUser) => roomUser.index) as [UUID, UUID]
+      );
+      roomUsers.forEach(({ index }) => connections[index].send(createGame));
+    }
+  } else {
+    console.error("There can be maximum 2 players in one room");
+  }
+};
+
+const handleAddShips = (ws: WebSocket, parsedData: Response, userId: UUID) => {
+  const gameId = toGameId(parsedData.data);
+  const playerShips = toShips(parsedData.data);
+  saveShips(gameId, userId, playerShips);
+  if (ships.get(gameId)?.size === 2) {
+    const players: [UUID, UUID] = games.get(gameId)!;
+    players.forEach((playerId) => {
+      const playerShips = ships.get(gameId)?.get(playerId)!;
+      const startGame = toRequest(
+        TYPE.START_GAME,
+        toStartGame(playerId, playerShips)
+      );
+      connections[playerId].send(startGame);
+    });
+  }
+};
